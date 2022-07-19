7ab2024fddae3ffd3d1dddc738d238d44fc60d25
Miracle Aghamobi <miracle.aghamobi@globalaccelerex.com>
2022-07-19 14:01:53 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: '/home', pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "sign-up", component: SignUpComponent},
+  {path: "login", component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

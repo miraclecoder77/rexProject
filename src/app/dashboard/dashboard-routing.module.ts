b5fb9f7ca6998449a11ecd6575191270f5efa514
Miracle Aghamobi <miracle.aghamobi@globalaccelerex.com>
2022-08-11 15:01:28 +0100

    dashboard sidebar link routing revamp

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -7,11 +7,11 @@ const routes: Routes = [{ path: '', component: DashboardComponent ,
   children: [
       {path: "", redirectTo: "home", pathMatch: "full"},
       {path: "home", component:DashboardHomeComponent},
-      { path: 'product', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+      { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
       { path: 'devices', loadChildren: () => import('./devices/devices.module').then(m => m.DevicesModule) },
       { path: 'reports', loadChildren: () => import('./reports/reports.module').then(m => m.ReportsModule) },
       { path: 'store', loadChildren: () => import('./store/store.module').then(m => m.StoreModule) },
-      { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+      { path: 'staff', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
       { path: 'paynow', loadChildren: () => import('./paynow/paynow.module').then(m => m.PaynowModule) },
       { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }
   ]

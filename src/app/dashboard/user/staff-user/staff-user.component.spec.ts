66ca1fd0441ec3404712f3c8dc12395f596b409a
Kehinde Alaga <kehinde.alaga@globalaccelerex.com>
2022-08-25 10:33:58 +0100

    products update & staff

---
+++ src/app/dashboard/user/staff-user/staff-user.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StaffUserComponent } from './staff-user.component';
+
+describe('StaffUserComponent', () => {
+  let component: StaffUserComponent;
+  let fixture: ComponentFixture<StaffUserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StaffUserComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StaffUserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
